e34547bf2acf3642315636a9d51397820df0d8b1
seven-1981 <41446512+seven-1981@users.noreply.github.com>
2018-07-20 14:09:11 +0200

    Update WAVFile.cpp

--- WAVFile/WAVFile.cpp
+++ WAVFile/WAVFile.cpp
@@ -1,8 +1,9 @@
 #include "WAVFile.h"
+#include "buffer.h"
 #include <iostream>
 #include <string>
 
-WAVFile::WAVFile(WAVHeader& header)
+WAVFile::WAVFile()
 {
 	//Initialize header - to be completed with "read_wav_file"
 	this->header = new WAVHeader;
@@ -11,8 +12,8 @@ WAVFile::WAVFile(WAVHeader& header)
 	this->header->header_size = PCM_WAV_FMT_LENGTH;
 	this->header->audio_format = PCM_WAV_FORMAT_TAG;
 	this->header->num_channels = PCM_WAV_CHANNELS;
-	this->header->sample_rate = header.sample_rate;
-	this->header->byte_rate = header.sample_rate * PCM_WAV_BITS * PCM_WAV_CHANNELS / 8;
+	this->header->sample_rate = 0;
+	this->header->byte_rate = 0;
 	this->header->frame_size = PCM_WAV_FRAME_SIZE;
 	this->header->bits_sample = PCM_WAV_BITS;
 	this->header->data_size = 0;
@@ -35,34 +36,38 @@ int WAVFile::read_wav_file(std::ifstream& file)
 	//Ignore the first four bytes
 	int ignore = read_word<int>(file, false);
 
+	//Check if it is a .wav file
+	if (ignore != 1380533830)
+		return -1;
+
 	//Now we read the total file size
 	unsigned int file_size = read_word<unsigned int>(file, true);
-	
+
 	//Ignore next 8 bytes - WAVE, fmt_
 	ignore = read_word<int>(file, false);
 	ignore = read_word<int>(file, false);
 
 	//Header size
 	unsigned int header_size = read_word<unsigned int>(file, true);
-	
+
 	//Get audio format and number of channels
 	short audio_format = read_word<short>(file, true);
 	short num_channels = read_word<short>(file, true);
-	
+
 	//Get sample rate and byte rate
 	unsigned int sample_rate = read_word<unsigned int>(file, true);
 	unsigned int byte_rate = read_word<unsigned int>(file, true);
-	
+
 	//Get frame size and bits per sample
 	short frame_size = read_word<short>(file, true);
 	short bits_sample = read_word<short>(file, true);
-	
+
 	//Now at last, get the data chunk size - first 4 bytes "data" ignored
 	ignore = read_word<int>(file, false);
 	unsigned int data_size = read_word<unsigned int>(file, true);
-	
+
 	//Create header struct
-	this->header->file_size = file_size;
+	this->header->file_size = file_size + 8;
 	this->header->header_size = header_size;
 	this->header->audio_format = audio_format;
 	this->header->num_channels = num_channels;
@@ -76,7 +81,7 @@ int WAVFile::read_wav_file(std::ifstream& file)
 
 	//Prepare buffer
 	this->data = new buffer<short>(data_size / frame_size, sample_rate);
-	
+
 	//Transfer data to buffer
 	for (long i = 0; i < (data_size / (unsigned int)frame_size); i++)
 		this->data->values[i] = read_word<short>(file, true);
@@ -88,6 +93,9 @@ int WAVFile::read_wav_file(std::ifstream& file)
 
 int WAVFile::write_wav_file(std::ofstream& file)
 {
+	if (this->header->file_size == 0)
+		return -1;
+
 	std::cout << "Writing wav file. " << std::endl;
 
 	//Write .wav file header
@@ -108,11 +116,9 @@ int WAVFile::write_wav_file(std::ofstream& file)
 	//Write the audio samples
 	for (long i = 0; i < this->data->size; i++)
 		write_word(file, (short)(this->data->values[i]), this->header->frame_size);
-	this->header->data_size = this->data->size;
 
 	//Final file size to fix the chunk size above
 	size_t file_length = file.tellp();
-	this->header->file_size = (unsigned int)file_length;
 
 	//Fix data chunk header to contain data size
 	file.seekp(data_chunk_pos + 4);
@@ -127,6 +133,12 @@ int WAVFile::write_wav_file(std::ofstream& file)
 	std::cout << "Success." << std::endl;
 
 	return 0;
+}
+
+void WAVFile::set_buffer(buffer<short>& data)
+{
+	this->data = &data;
+	create_header_info();
 }
 
 template <typename Word>
@@ -170,6 +182,15 @@ std::ostream& WAVFile::write_word(std::ostream& out_stream, Word value, unsigned
 	for (; size != 0; --size, value >>= 8)
 		out_stream.put(static_cast<char>(value & 0xFF));
 	return out_stream;
+}
+
+void WAVFile::create_header_info()
+{
+	//The buffer has been set - calculate missing .wav header info
+	this->header->sample_rate = this->data->sample_rate;
+	this->header->byte_rate = this->data->sample_rate * PCM_WAV_FRAME_SIZE;
+	this->header->data_size = this->data->size * PCM_WAV_FRAME_SIZE;
+	this->header->file_size = this->header->data_size + PCM_WAV_HEADER_SIZE;
 }
 
 void WAVFile::dump_info()
