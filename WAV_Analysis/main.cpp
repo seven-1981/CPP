cf4c8333e972b84b3e44cb90209a2997877f0876
seven-1981 <41446512+seven-1981@users.noreply.github.com>
2018-07-25 16:05:26 +0200

    Update main.cpp

--- WAV_Analysis/main.cpp
+++ WAV_Analysis/main.cpp
@@ -3,12 +3,11 @@
 #include <vector>
 
 #include "WAVFile.h"
-#include "filt.h"
 #include "DSP.h"
 //Uses boost library. In Project options set include path to c:\program files\boost
 //and library path to C:\Program Files\Boost\stage\lib
 //boost lib files must be created first with b2.exe and bjam.exe
-#include "gnuplot-iostream.h"
+//#include "gnuplot-iostream.h"
 
 //Defines for audio parameters
 #define PCM_BUF_SIZE       88200
@@ -21,10 +20,10 @@
 #define MOV_AVG_SIZE 50
 
 //Defines for Plots
-#define PLOT_WAVFILE
-#define PLOT_FFT
+//#define PLOT_WAVFILE
+//#define PLOT_FFT
 //#define PLOT_INV_FFT
-#define PLOT_AUTOCORR
+//#define PLOT_AUTOCORR
 
 double extract_bpm_value(buffer<double>& autocorr_array, buffer<short>& inbuffer, buffer<double>& mov_avg)
 {
@@ -79,7 +78,7 @@ int main()
 {
 	//WAV FILE READ
 	WAVFile wavfile;
-	std::ifstream read_wav("mdh7_1.wav", std::ios_base::in | std::ios_base::binary);
+	std::ifstream read_wav("./wavs/sinewave1000Hz.wav", std::ios_base::in | std::ios_base::binary);
 	wavfile.read_wav_file(read_wav);
 	read_wav.close();
 	std::cout << "The last byte is: " << wavfile.get_buffer()->values[PCM_BUF_SIZE - 1] << std::endl;
@@ -88,7 +87,7 @@ int main()
 	//CREATE WINDOW BUFFER AND APPLY WINDOW
 	buffer<double> window_buffer(wavfile.get_size(), sample_rate);
 	apply_window(*wavfile.get_buffer(), window_buffer, tukey, 0.5);
-	
+
 	//PLOT WINDOWED WAVFILE
 #ifdef PLOT_WAVFILE
 	Gnuplot gp1("\"C:\\Programme\\gnuplot\\bin\\gnuplot.exe -persist\"");
@@ -98,18 +97,23 @@ int main()
 	for (long i = 0; i < size1; i++)
 		data1.push_back(window_buffer.values[i]);
 	gp1.send1d(data1);
+#else
+	std::ofstream windowdata("windata.txt", std::ios_base::out | std::ios_base::trunc);
+	for (long i = 0; i < window_buffer.get_size(); i++)
+		windowdata << window_buffer.values[i] << "\n";
+	windowdata.close();
 #endif
-	
+
 	//APPLY FFT TO WAV AND PLOT
-	long pow2samples = pow2_size(window_buffer);
+	long pow2samples = pow2_size(window_buffer, true); // true for floor, false for ceil
 	std::cout << "Pow 2 size: " << pow2samples << "." << std::endl;
 	buffer<short> time_domain(pow2samples, sample_rate);
 	buffer<double> freq_domain(pow2samples * 2, sample_rate);
 	create_fft_buffer(*wavfile.get_buffer(), time_domain);
-	//create_fft_buffer(window_buffer, time_domain); <- uses windowed data
+	//create_fft_buffer(window_buffer, time_domain); //<- uses windowed data
 	perform_fft(time_domain, freq_domain, +1);
 
-	double freqres = (double)freq_domain.get_sample_rate() / freq_domain.get_size();
+	double freqres = (double)freq_domain.get_sample_rate() / pow2samples;
 	std::cout << "The freq. resolution: " << freqres << std::endl;
 #ifdef PLOT_FFT
 	Gnuplot gp2("\"C:\\Programme\\gnuplot\\bin\\gnuplot.exe -persist\"");
@@ -118,11 +122,16 @@ int main()
 	for (long i = 0; i < 5000; i++)
 		pair.push_back(std::pair<double, double>((double)i*freqres, 1.0 / (pow2samples * 2) * sqrt(freq_domain.values[i] * freq_domain.values[i] + freq_domain.values[i + 1] * freq_domain.values[i + 1])));
 	gp2.send1d(pair);
+#else
+	std::ofstream fftdata("fftdata.txt", std::ios_base::out | std::ios_base::trunc);
+	for (long i = 0; i < pow2samples; i+=2)
+		fftdata << (double)i / 2 * freqres << ", " << 1.0 / (pow2samples) * sqrt(freq_domain.values[i] * freq_domain.values[i] + freq_domain.values[i + 1] * freq_domain.values[i + 1]) << "\n";
+	fftdata.close();
 #endif
 
 	//REMOVE BASS FREQ FROM FFT
 	buffer<double> freq_filt(pow2samples, sample_rate);
-	lowcut_freq(freq_domain, freq_filt, 250.0);
+	cut_freq(freq_domain, freq_filt, 20.0, 250.0);
 
 	//INVERSE FFT AND PLOT
 	buffer<double> time_filt(pow2samples * 2, sample_rate);
@@ -140,7 +149,7 @@ int main()
 	//EXTRACT WAV DATA
 	buffer<short> endbuffer(pow2samples, sample_rate);
 	for (long i = 0; i < pow2samples; i++)
-		endbuffer.values[i] = (short)(1.0/(double)sample_rate * time_filt.values[i]);
+		endbuffer.values[i] = (short)(1.0 / (double)sample_rate * time_filt.values[i]);
 
 	//MAXIMIZE VOLUME OF DATA
 	maximize_volume(endbuffer);
@@ -148,21 +157,21 @@ int main()
 	//WRITE FILTERED WAV FILE
 	WAVFile filt_wav;
 	filt_wav.set_buffer(endbuffer);
-	std::ofstream filtwav("filtwav.wav", std::ios_base::out | std::ios_base::binary);
+	std::ofstream filtwav("./wavs/filtwav.wav", std::ios_base::out | std::ios_base::trunc | std::ios_base::binary);
 	filt_wav.write_wav_file(filtwav);
 	filtwav.close();
-	
+
 	//PERFORM AUTOCORRELATION AND EXTRACT BPM
 	buffer<double> autocorr_array(AUTOCORR_RES, PCM_SAMPLE_RATE);
 	buffer<double> mov_avg(AUTOCORR_RES - MOV_AVG_SIZE, PCM_SAMPLE_RATE);
 	double value = extract_bpm_value(autocorr_array, endbuffer, mov_avg);
-	
+
 	//ENVELOPE FILTER ON AUTOCORR
 	buffer<double> env(AUTOCORR_RES, PCM_SAMPLE_RATE);
 	envelope_filter(autocorr_array, env, 0.005);
 
 	std::cout << "The BPM value is: " << value << std::endl;
-	
+
 	//AUTOCORR PLOT
 #ifdef PLOT_AUTOCORR
 	Gnuplot gp4("\"C:\\Programme\\gnuplot\\bin\\gnuplot.exe -persist\"");
@@ -173,5 +182,5 @@ int main()
 	gp4.send1d(data4);
 #endif
 
-	return 0;
+return 0;
 }
