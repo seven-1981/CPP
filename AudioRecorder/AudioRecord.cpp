0014785cb748bad6fd58181e3e5a00c372329290
seven-1981 <41446512+seven-1981@users.noreply.github.com>
2018-07-31 15:03:21 +0200

    Update AudioRecord.cpp

--- AudioRecorder/AudioRecord.cpp
+++ AudioRecorder/AudioRecord.cpp
@@ -11,22 +11,26 @@ AudioRecord::AudioRecord(long sample_rate, long duration)
 	this->wfx.wBitsPerSample = PCM_WAV_BITS;
 
 	// Allocate some memory for the wav data
-	this->waveIn[0] = (short*)malloc(duration * sample_rate * sizeof(short));
-	this->waveIn[1] = (short*)malloc(duration * sample_rate * sizeof(short));
-	std::cout << "Allocated 2 x " << duration * sample_rate * sizeof(short) << " bytes for audio record data." << std::endl;
+	for (int i = 0; i < NUM_OF_BUFFERS; i++)
+		this->waveIn[i] = (short*)malloc(duration * sample_rate * sizeof(short));
+	std::cout << "Allocated " << NUM_OF_BUFFERS << "x " << duration * sample_rate * sizeof(short) << " bytes for audio record data." << std::endl;
 
 	// Set up and prepare headers for input
 	for (int i = 0; i < NUM_OF_BUFFERS; i++)
 	{
 		this->WaveInHdr[i].lpData = (LPSTR)this->waveIn[i];
-		this->WaveInHdr[i].dwBufferLength = duration * sample_rate;
+		this->WaveInHdr[i].dwBufferLength = duration * sample_rate * sizeof(short);
 		this->WaveInHdr[i].dwBytesRecorded = 0;
 		this->WaveInHdr[i].dwUser = 0L;
 		this->WaveInHdr[i].dwFlags = 0L;
 		this->WaveInHdr[i].dwLoops = 0L;
 	}
 
+	// Set result to zero
 	this->result = 0;
+
+	// Call initialisation
+	this->init();
 }
 
 AudioRecord::~AudioRecord()
@@ -42,10 +46,47 @@ AudioRecord::~AudioRecord()
 	std::cout << "Releasing resources. Freed audio record memory." << std::endl;
 }
 
+MMRESULT AudioRecord::start()
+{
+	if (this->result != 0)
+		return this->result;
+
+	this->result = waveInStart(this->hWaveIn);
+
+	return result;
+}
+
+bool AudioRecord::is_full()
+{
+	bool buf_retval[NUM_OF_BUFFERS];
+	bool retVal = true;
+
+	for (int i = 0; i < NUM_OF_BUFFERS; i++)
+	{
+		buf_retval[i] = this->WaveInHdr[i].dwFlags & WHDR_DONE;
+		retVal = buf_retval[i] && retVal;
+	}
+
+	return retVal;
+}
+
+bool AudioRecord::is_full(int index)
+{
+	if (index < NUM_OF_BUFFERS)
+		return (bool)(this->WaveInHdr[index].dwFlags & WHDR_DONE);
+	else
+		return false;
+}
+
+short* AudioRecord::flush(int index)
+{
+	return this->waveIn[index];
+}
+
 MMRESULT AudioRecord::init()
 {
 	// Open waveform input recording device - fills hWaveIn
-	this->result = waveInOpen(&this->hWaveIn, WAVE_MAPPER, &this->wfx, (DWORD_PTR)(AudioRecord::buffer_callback), NULL, CALLBACK_FUNCTION | WAVE_FORMAT_DIRECT);
+	this->result = waveInOpen(&this->hWaveIn, WAVE_MAPPER, &this->wfx, NULL, NULL, CALLBACK_NULL | WAVE_FORMAT_DIRECT);
 	if (this->result != 0)
 		return this->result;
 
@@ -65,44 +106,4 @@ MMRESULT AudioRecord::init()
 	std::cout << "Initialization done." << std::endl;
 
 	return this->result;
-}
-
-MMRESULT AudioRecord::start()
-{
-	if (this->result != 0)
-		return this->result;
-
-	this->result = waveInStart(this->hWaveIn);
-
-	return result;
-}
-
-bool AudioRecord::full()
-{
-	bool retVal = false;
-	if ((this->WaveInHdr[0].dwFlags & WHDR_DONE) && (this->WaveInHdr[1].dwFlags & WHDR_DONE))
-			retVal = true;
-
-	return retVal;
-}
-
-MMRESULT AudioRecord::stop()
-{
-	if (this->result != 0)
-		return this->result;
-	
-	this->result = waveInStop(this->hWaveIn);
-
-	return result;
-}
-
-short* AudioRecord::flush(int index)
-{
-	return this->waveIn[index];
-}
-
-void CALLBACK AudioRecord::buffer_callback(HWAVEIN hwi, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
-{
-	if (uMsg == WIM_DATA)
-		std::cout << ".";
 }
