292069d90204a4de4208aea29ea52547b0ee2ce3
seven-1981 <41446512+seven-1981@users.noreply.github.com>
2019-04-07 13:09:55 +0200

    Add files via upload

--- Display_OpenGL/FCWindowSpectrum.cpp
+++ Display_OpenGL/FCWindowSpectrum.cpp
@@ -1,35 +1,45 @@
 #include "FCWindowSpectrum.hpp"
+#include "FCWindowAxis.hpp"
 
 #include <GL/freeglut.h>
+#include <cmath>
+#include <iostream>
+
+//Graphic drawing constants
+const int SPECTRUM_SIZE_X_MIN = 300;
+const int SPECTRUM_SIZE_Y_MIN = 150;
+
+//Cycle time measurement
+//#define ENABLE_CYCLE_TIME_MEASUREMENT
 
 //***********************************************************************************************
 // FCWindowSpectrum
 //***********************************************************************************************
 
-FCWindowSpectrum::FCWindowSpectrum(int x, int y, std::string title, int size, bool fullscreen)
- : FCWindow(x, y, title, fullscreen)
+FCWindowSpectrum::FCWindowSpectrum(FCWindowParam_t& param)
+ : FCWindow(param)
 {
 	//Initialize derived type specific data
 	this->font = GLUT_BITMAP_TIMES_ROMAN_24;
-	this->size = size;
+	this->size = param.size;
+	this->params_calculated = false;
+	//Initialize vector
+	for (int i = 0; i < this->size; i++)
+		this->data.values.push_back(0);
 
-	//Graphic default parameters
+	//Adjustable graphic parameters
 	this->x_border = 50;
 	this->y_border_bottom = 50;
 	this->y_border_top = 100;
-	this->x_margin = 5;
+	this->bar_width_frac = 0.5;
 	this->max_value = 100.0;
 	this->min_value = 0.0;
-	this->n_ticks = 11;
-	this->tick_width = 5;
-	
-	//Update static map (handle and instance)
-	FCWindow::static_data.mtx.lock();
-	FCWindow::static_data.windows.insert(std::pair<int, FCWindowSpectrum*>(this->handle, this));
-	FCWindow::static_data.mtx.unlock();
 
-	//Draw static content
+	//Draw static content and calculate parameters
 	this->init();	
+	
+	//Initialize timer object
+	this->tim.init();
 }
 
 FCWindowSpectrum::~FCWindowSpectrum()
@@ -44,32 +54,36 @@ void FCWindowSpectrum::update(FCWindowData_t* data)
 	//Protect data
 	this->spectrum_mutex.lock();
 	for (int i = 0; i < this->size; i++)
-		this->data.values[i] = pWindowData->values[i];
-	this->spectrum_mutex.unlock();	
+		this->data.values.at(i) = pWindowData->values.at(i);
+	this->spectrum_mutex.unlock();
 }
 
 void FCWindowSpectrum::display_(void)
 {
+	//Check flag
+	if (this->params_calculated == false)
+		return;
+		
 	//Specific member callback (called from static base 'display')
-	glutSetWindow(this->handle);
+	glutSetWindow(this->get_handle());
 
 	//Specify window for clearance
-	glScissor(this->x_border, this->y_border_bottom, this->x - 2 * this->x_border, y_diff);
+	//Protect data
+  	this->spectrum_mutex.lock();
+	glScissor(this->x_border, this->y_border_bottom, this->get_width() - 2 * this->x_border, this->y_diff);
 	glEnable(GL_SCISSOR_TEST);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glScissor(0, 0, FCWindow::static_data.res_x, FCWindow::static_data.res_y);
+	glScissor(0, 0, FCWindowManager::get_res_x(), FCWindowManager::get_res_y());
 	glDisable(GL_SCISSOR_TEST);
-
+	
   	//Display spectrum
-  	//Protect data
-  	this->spectrum_mutex.lock();
 	for (int i = 0; i < this->size; i++)
 	{
 		//Get data from array and calculate height
 		int x_bar = this->x_begin + i * this->x_diff;
 		int x_bar2 = x_bar + this->x_size;
 
-		double lim_val = std::max(this->data.values[i], this->min_value);
+		double lim_val = std::max(this->data.values.at(i), this->min_value);
 		lim_val = std::min(lim_val, this->max_value);
 		int y_bar2 = y_bar - (int)(lim_val * m + n);
 
@@ -80,21 +94,33 @@ void FCWindowSpectrum::display_(void)
 		glVertex3f(x_bar2, y_bar2, 0.0);
 		glVertex3f(x_bar, y_bar2, 0.0);
 		glEnd();
-
-		//Draw labels on x axis
-		this->font = GLUT_BITMAP_TIMES_ROMAN_10;
-		if (i % 2 == 0)
-			output(x_bar, y_bar + 20, std::to_string(i * 43));
 	}
+	
 	this->spectrum_mutex.unlock();
+	
+	//Display timing info
+	#ifdef ENABLE_CYCLE_TIME_MEASUREMENT
+		long long t = this->tim.get_time_us();
+		this->font = GLUT_BITMAP_HELVETICA_12;
+		this->output(350, 120, std::to_string(t));
+	#endif
 
   	glutSwapBuffers();
 	glutPostRedisplay();
 }
 
+void FCWindowSpectrum::reshape_(int width, int height)
+{
+	//Specific member callback (called from static base 'reshape')
+	//Overridden, because we additionally need to call init()
+  	FCWindow::reshape_(width, height);
+  	//Init static content
+  	this->init();
+}
+
 void FCWindowSpectrum::output(int x, int y, std::string text)
 {
-	glutSetWindow(this->handle);
+	glutSetWindow(this->get_handle());
   	int len, i;
   	glRasterPos2f(x, y);
   	len = text.length();
@@ -108,35 +134,89 @@ void FCWindowSpectrum::init()
 {
 	//Display title
 	this->font = GLUT_BITMAP_TIMES_ROMAN_24;
-	this->output(50, 50, "* * * AUDIO SPECTROGRAM * * *");
+	this->output(50, 50, "* * * AUDIO SPECTRUM * * *");
 
 	//Calculate auxiliary parameters
-	this->x_size = (this->x - 2 * this->x_border - ((this->size - 1) * this->x_margin)) / this->size;
+	this->calc_param();
+	
+	//Draw axes
+	this->draw_axes();
+}
+
+void FCWindowSpectrum::calc_param()
+{
+	//Reset flag
+	this->params_calculated = false;
+	
+	//Calculate auxiliary parameters
+	//Check if desired parameters fit into window
+	int x_width = this->get_width() - 2 * this->x_border;
+	int y_width = this->get_height() - this->y_border_bottom - this->y_border_top;
+	if (x_width < SPECTRUM_SIZE_X_MIN)
+		return;
+	if (y_width < SPECTRUM_SIZE_Y_MIN)
+		return;
+	
+	std::cout << "x_width = " << x_width << std::endl;
+	std::cout << "y_width = " << y_width << std::endl;
+	
+	//Determine max number of bars
+	this->max_n_bars = (int)std::ceil((double)x_width / 2);
+	std::cout << "max_n_bars = " << max_n_bars << std::endl;
+	
+	//Determine number of bars
+	if (this->size > this->max_n_bars)
+		this->n_bars = this->max_n_bars;
+	else
+		this->n_bars = this->size;
+	std::cout << "n_bars = " << this->n_bars << std::endl;
+	
+	//Calculate margin and size of one spectrum bar
+	int bar_margin = x_width / this->n_bars;
+	std::cout << "bar+margin = " << bar_margin << std::endl;
+
+	//this->x_size = (this->x - 2 * this->x_border - ((this->size - 1) * this->x_margin)) / this->size;
+	this->x_size = (int)((double)bar_margin * this->bar_width_frac);
+	this->x_margin = bar_margin - this->x_size;
+	std::cout << "x_size = " << this->x_size << std::endl;
+	std::cout << "x_margin = " << this->x_margin << std::endl;
+	
+	//Calculate coordinates for bars
 	this->x_begin = this->x_border;
 	this->x_diff =this->x_size + this->x_margin;
-	this->y_bar = this->y - this->y_border_bottom;
-	this->y_diff = this->y - this->y_border_bottom - this->y_border_top;
+	this->y_bar = this->get_height() - this->y_border_bottom;
+	this->y_diff = this->get_height() - this->y_border_bottom - this->y_border_top;
 	this->m = (double)this->y_diff / (double)(this->max_value - this->min_value);
 	this->n = -this->m * (double)this->min_value;
+	std::cout << "x_begin = " << this->x_begin << std::endl;
+	std::cout << "x_diff = " << this->x_diff << std::endl;
+	std::cout << "y_bar = " << this->y_bar << std::endl;
+	std::cout << "y_diff = " << this->y_diff << std::endl;
+	std::cout << "m = " << this->m << std::endl;
+	std::cout << "n = " << this->n << std::endl;
+	
+	//Calculation finished, set flag
+	this->params_calculated = true;
+}
 
-	//Draw scale on y axis
-	glLineWidth(1.0);
-	int tick = this->y_diff / (this->n_ticks - 1);
-	for (int i = 0; i < this->n_ticks; i++)
-	{
-		glBegin(GL_LINES);
-		glVertex3f(this->x_begin - this->tick_width, this->y_bar - i * tick, 0.0);
-		glVertex3f(this->x_begin,                    this->y_bar - i * tick, 0.0);
-		glEnd();
-	}
-
-	//Draw labels on y axis
-	this->font = GLUT_BITMAP_TIMES_ROMAN_10;
-	double inc = (this->max_value - this->min_value) / (double)(this->n_ticks - 1);
-	for (int i = 0; i < this->n_ticks; i++)
-	{
-		output(this->x_begin - this->tick_width - 20, this->y_bar - i * tick, std::to_string((int)(this->min_value + i * inc)));
-	}
+void FCWindowSpectrum::draw_axes(void)
+{
+	//Draw x axis
+	FCWindowAxisData_t axis;
+	axis.x = this->x_begin; axis.y = this->y_bar;
+	axis.min = 0.0; axis.max = 2000.0;
+	axis.N = 11;
+	axis.span = this->get_width() - 2 * this->x_border;
+	FCWindowAxis x_axis(AxisTypeX, GLUT_BITMAP_TIMES_ROMAN_10, axis);
+	x_axis.draw();
+	
+	//Draw y axis
+	axis.x = this->x_begin; axis.y = this->y_bar;
+	axis.min = -100.0; axis.max = 0.0;
+	axis.N = 11;
+	axis.span = this->get_height() - this->y_border_top - this->y_border_bottom;
+	FCWindowAxis y_axis(AxisTypeY, GLUT_BITMAP_TIMES_ROMAN_10, axis);
+	y_axis.draw();
 }
 
 void FCWindowSpectrum::set_param(FCWindowSpectrumSize_t& param)
@@ -144,11 +224,13 @@ void FCWindowSpectrum::set_param(FCWindowSpectrumSize_t& param)
 	this->x_border	      = param.x_border;
 	this->y_border_bottom = param.y_border_bottom;
 	this->y_border_top    = param.y_border_top;
-	this->x_margin 	      = param.x_margin;
+	
+	this->bar_width_frac  = param.bar_width_frac;
 
 	this->max_value       = param.max_value;
 	this->min_value       = param.min_value;
 
-	this->n_ticks	      = param.n_ticks;
-	this->tick_width      = param.tick_width;
+	//Redraw static content
+	glClear(GL_COLOR_BUFFER_BIT);
+	this->init();
 }
--- Display_OpenGL/FCWindowSpectrum.hpp
+++ Display_OpenGL/FCWindowSpectrum.hpp
@@ -1,40 +1,46 @@
-#ifndef _FC_WINDOWSPECTRUM
-#define _FC_WINDOWSPECTRUM
+#ifndef _FC_WINDOWSPECTRUM_H
+#define _FC_WINDOWSPECTRUM_H
 
 #include "FCWindow.hpp"
+#include "Timing.hpp"
 
 #include <string>
 #include <mutex>
+#include <vector>
 
 struct FCWindowSpectrumSize_t
 {
 	int x_border = 50;
 	int y_border_bottom = 50;
 	int y_border_top = 100;
-	int x_margin = 5;
+	
+	double bar_width_frac = 0.5;
 
 	double max_value = 100.0;
 	double min_value = 0.0;
-
-	int n_ticks = 11;
-	int tick_width = 5;
 };
 
 //Inherit from base data struct
 struct FCWindowSpectrumData_t : public FCWindowData_t
 {
-	double* values;
+	std::vector<double> values;
 };
 
 //Frequency spectrum display
 class FCWindowSpectrum : public FCWindow
 {
 public:
-	explicit FCWindowSpectrum(int, int, std::string, int, bool);
 	~FCWindowSpectrum();
 
 	void update(FCWindowData_t*);
 	void set_param(FCWindowSpectrumSize_t&);
+	
+	//FCWindowManager must be able to access FCWindowSpectrum
+	friend class FCWindowManager;
+	
+protected:
+	//Protected constructor (factory method)
+	explicit FCWindowSpectrum(FCWindowParam_t&);
 
 private:
 	//Font used on this window
@@ -45,33 +51,45 @@ private:
 	//Data for frequency array
 	int size;
 	FCWindowSpectrumData_t data;
+	//Flag indicates that params have been calculated
+	bool params_calculated;
 	
-	//Graphic parameters
-	int x_border;
-	int y_border_bottom;
-	int y_border_top;
-	int x_margin;
-	double max_value;
-	double min_value;
-	int n_ticks;
-	int tick_width;
+	//Adjustable graphic parameters
+	int x_border;			//Distance window border - spectrum sides
+	int y_border_bottom;	//Distance window border - spectrum bottom
+	int y_border_top;		//Distance window border - spectrum top
+	double bar_width_frac;	//Fraction bar width / bar margin
+	double max_value;		//Minimal y axis value
+	double min_value;		//Maximal y axis value
 
 	//Auxiliary parameters
-	int x_size;
-	int x_begin;
-	int x_diff;
-	int y_bar;
-	int y_diff;
-	double m;
-	double n;
+	int max_n_bars;			//Maximum number of bars	
+	int n_bars;				//Number of bars
+	int x_size;				//Width of one bar
+	int x_margin;			//Space between two bars
+	int x_begin;			//First bar x coordinates
+	int x_diff;				//Increment for next bar
+	int y_bar;				//Bottom bars coordinates
+	int y_diff;				//Width of y axis
+	double m;				//Gain for scale
+	double n;				//Offset for scale
 
 	//Helper function for displaying text
 	void output(int, int, std::string);
-	//Helper function to initialize non spectrum content
+	//Helper function to display non spectrum content, axes
+	//and calculate parameters
 	void init();
+	//Calculate auxiliary parameters
+	void calc_param();
+	//Draw axes
+	void draw_axes();
 	
 	//Member callbacks - must be overridden by derived class
 	void display_(void);
+	void reshape_(int, int);
+	
+	//Timer object
+	Timing tim;
 };
 
 #endif
