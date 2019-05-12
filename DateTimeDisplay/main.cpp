fd4ca8f896b2b9ba183df7fdf86d0170f8e480fc
seven-1981 <41446512+seven-1981@users.noreply.github.com>
2019-05-12 22:29:52 +0200

    Update main.cpp

--- DateTimeDisplay/main.cpp
+++ DateTimeDisplay/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <map>
+#include <random>
 
 #include "FCWindow.hpp"
 #include "FCWindowManager.hpp"
@@ -23,17 +24,27 @@ const std::string TITLE_DISP = "* PI - HOLE *";
 //Constants for location of text
 const int TITLE_X = 240; 
 const int TITLE_Y = 50;
-const int X_MARGIN = 650; 
-const int Y_MARGIN = 150; 
-const int Y_OFFSET = 100;
-const int Y_LINE_OFFSET = 40;
+const int X_MARGIN = 50; 
+const int X_MARGIN_STATION = 350;
+const int X_MARGIN_SECOND_ROW = 600; 
+const int Y_MARGIN = 100; 
+const int Y_LINE_OFFSET = 43;
 const int SECONDS_WAIT = 3;
 //URL for http weather request - sample
 const std::string url_openweathermap = "https://samples.openweathermap.org/data/2.5/weather?lat=47.319542&lon=8.051965&appid=b6907d289e10d714a6e88b30761fae22";
 //URL for http weather request - real data
-const std::string url_req = "https://api.openweathermap.org/data/2.5/weather?lat=47.319542&lon=8.051965&appid=889a052df8efc1c1c0235058b557c1c9";
-//Constants for weather collection
-const int CYCLE_REQUEST = 60;
+const std::string url_req = "https://api.openweathermap.org/data/2.5/weather?lat=47.319542&lon=8.051965&appid=889a052df8efc1c1c0235058b557c1c9&lang=de";
+//Constants for coordinate selection
+const int LAT_MIN = 46.731;
+const int LAT_MAX = 47.580;
+const int LON_MIN = 7.0400;
+const int LON_MAX = 9.4111;
+//Random generator seed
+std::random_device rd;
+std::mt19937 gen(rd());
+//Uniform distribution for coordinates
+std::uniform_real_distribution<> dis_lat(LAT_MIN, LAT_MAX);
+std::uniform_real_distribution<> dis_lon(LON_MIN, LON_MAX);	
 
 //Weather item holding data for parsing and display
 //info whether it is a value or a string
@@ -50,16 +61,16 @@ const int NUM_WEATHER_ITEMS = 10;
 //Fill map with desired items to display
 void init_map()
 {
-	WeatherItem weather_cond { "description", "WEATHER"       , ""     , false };
-	WeatherItem temperature  { "temp"       , "TEMPERATURE"   , " degC", true };
-	WeatherItem temp_min     { "temp_min"   , "TEMP. MIN"     , " degC", true };
-	WeatherItem temp_max     { "temp_max"   , "TEMP. MAX"     , " degC", true };
-	WeatherItem pressure     { "pressure"   , "PRESSURE"      , " hPa" , true };
-	WeatherItem humidity     { "humidity"   , "HUMIDITY"      , " %"   , true };
-	WeatherItem wind_speed   { "speed"      , "WIND SPEED"    , " m/s" , true };
-	WeatherItem wind_dir     { "deg"        , "WIND DIRECTION", " deg" , true };
-	WeatherItem cloudiness   { "all"        , "CLOUDINESS"    , " %"   , true };
-	WeatherItem station      { "name"       , "STATION"       , ""     , false };
+	WeatherItem weather_cond { "description", ""              , ""     , false };
+	WeatherItem temperature  { "temp"       , "TEMPERATUR:"   , " degC", true };
+	WeatherItem temp_min     { "temp_min"   , "TEMP. MIN:"    , " degC", true };
+	WeatherItem temp_max     { "temp_max"   , "TEMP. MAX:"    , " degC", true };
+	WeatherItem pressure     { "pressure"   , "LUFTDRUCK:"    , " hPa" , true };
+	WeatherItem humidity     { "humidity"   , "FEUCHTE:"      , " %"   , true };
+	WeatherItem wind_speed   { "speed"      , "WINDSTAERKE:"  , " m/s" , true };
+	WeatherItem wind_dir     { "deg"        , "WINDRICHTUNG:" , " deg" , true };
+	WeatherItem cloudiness   { "all"        , "WOLKENDECKE:"  , " %"   , true };
+	WeatherItem station      { "name"       , "STATION: "     , ""     , false };
 	weather_items.emplace(0, weather_cond);
 	weather_items.emplace(1, temperature);
 	weather_items.emplace(2, temp_min);
@@ -70,6 +81,33 @@ void init_map()
 	weather_items.emplace(7, wind_dir);
 	weather_items.emplace(8, cloudiness);
 	weather_items.emplace(9, station);
+}
+
+double get_random(bool lat) 
+{
+	//Returns a random double between min and max
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	if (lat == true)
+		return (double)dis_lat(gen);
+	else
+		return (double)dis_lon(gen);
+}
+
+void random_url_pos(std::string& url)
+{
+	double lat = get_random(true);
+	double lon = get_random(false);
+	std::string lat_str = std::to_string(lat);
+	std::string lon_str = std::to_string(lon);
+	std::size_t lat_pos = url.find("lat=");
+	std::size_t lat_end = url.find("&", lat_pos + 1);
+	std::size_t lat_len = lat_end - lat_pos - 4;
+	std::size_t lon_pos = url.find("lon=");
+	std::size_t lon_end = url.find("&", lon_pos + 1);
+	std::size_t lon_len = lon_end - lon_pos - 4;
+	url = url.replace(lat_pos + 4, lat_len, lat_str);
+	url = url.replace(lon_pos + 4, lon_len, lon_str);
 }
 
 //Create date and time string
@@ -136,10 +174,11 @@ int main(int argc, char **argv)
 	WeatherCollector collector(url_req);
 	std::string weather_data { };
 	unsigned int main_cycle = 0;
-	unsigned int weather_cycle = 0;
 	bool successful = false;
 	init_map();
+
 	//Initialize window manager and start window
+
   	FCWindowManager::init(argc, argv);
 	FCWindowParam_t win_param { 500, 250, TITLE, true };
 	FCWindow* window1 = FCWindowManager::create(TypeWindowLabel, win_param);
@@ -148,72 +187,56 @@ int main(int argc, char **argv)
 	
 	while (window1->get_quit() == false)
 	{
-		if (main_cycle % 2 == 1) //First cycle must initialize weather data
+		//Create timestamp and time string
+		std::string date, time;
+		create_timestamp(date, time);
+		date = "DATE: " + date;
+		time = "TIME: " + time;
+		//Create text item data for window
+		FCWindowLabelData_t display_data;
+		FCWindowLabelDataItem_t element;
+		//Create title
+		element.text = TITLE_DISP;
+		element.x = TITLE_X; element.y = TITLE_Y;
+		element.use_stroke = true;
+		display_data.items.push_back(element);
+		//Create date & time and random color
+		element.text = date;
+		element.x = X_MARGIN;
+		element.y = Y_MARGIN;
+		element.color_R = (float)(rand()) / (float)RAND_MAX;
+		element.color_G = (float)(rand()) / (float)RAND_MAX;
+		element.color_B = (float)(rand()) / (float)RAND_MAX;
+		display_data.items.push_back(element);
+		element.text = time;
+		element.y += Y_LINE_OFFSET;
+		display_data.items.push_back(element);
+		
+		//Create weather data
+		std::string url_to_request = url_req;
+		if (main_cycle % 2 == 0)
 		{
-			//Create timestamp and time string
-			std::string date, time;
-			create_timestamp(date, time);
-			date = "DATE: " + date;
-			time = "TIME: " + time;
-
-			//Create text item data for window
-			FCWindowLabelData_t timedata;
-			FCWindowLabelDataItem_t element;
-			//Create title
-			element.text = TITLE_DISP;
-			element.x = TITLE_X; element.y = TITLE_Y;
-			element.use_stroke = true;
-			timedata.items.push_back(element);
-			//Create date & time and random pos and color
-			int rx = FCWindowManager::get_res_x();
-			int ry = FCWindowManager::get_res_y();
-			element.text = date;
-			element.x = rand() % (rx-X_MARGIN); 
-			element.y = rand() % (ry-Y_MARGIN) + Y_OFFSET;
-			element.color_R = (float)(rand()) / (float)RAND_MAX;
-			element.color_G = (float)(rand()) / (float)RAND_MAX;
-			element.color_B = (float)(rand()) / (float)RAND_MAX;
-			timedata.items.push_back(element);
-			element.text = time;
-			element.y = element.y + Y_LINE_OFFSET;
-			timedata.items.push_back(element);
-			
-			//Update window data
-			window1->update(&timedata);
+			//Every second cycle, display some random location's weather data
+			random_url_pos(url_to_request);
 		}
-		else
+		collector.update_url(url_to_request);
+		bool successful = collector.request(weather_data);
+		//std::cout << weather_data << std::endl;
+		
+		if (successful == true)
 		{
-			if (main_cycle % CYCLE_REQUEST == 0)
+			for (int i = 0; i < NUM_WEATHER_ITEMS; i++)
 			{
-				//Update weather data every n'th cycle only
-				successful = collector.request(weather_data);
-				//std::cout << weather_data << std::endl;
-			}
-			if (successful == true)
-			{
-				FCWindowLabelData_t weatherdata;
-				FCWindowLabelDataItem_t element;
-				//Create title
-				element.text = TITLE_DISP;
-				element.x = TITLE_X; element.y = TITLE_Y;
-				element.use_stroke = true;
-				weatherdata.items.push_back(element);
-				//Create weather data
-				int rx = FCWindowManager::get_res_x();
-				int ry = FCWindowManager::get_res_y();
 				//Get map entry according to cycle
-				auto found = weather_items.find(weather_cycle);
+				auto found = weather_items.find(i);
 				if (found == weather_items.end())
-					continue; //Not found - display date & time only
+					continue; //Not found
 				WeatherItem item = found->second;
-				//Create weather information and random pos and color
+				//Create weather information and location
 				element.text = item.title;
-				element.x = rand() % (rx-X_MARGIN); 
-				element.y = rand() % (ry-Y_MARGIN) + Y_OFFSET;
-				element.color_R = (float)(rand()) / (float)RAND_MAX;
-				element.color_G = (float)(rand()) / (float)RAND_MAX;
-				element.color_B = (float)(rand()) / (float)RAND_MAX;
-				weatherdata.items.push_back(element);
+				element.x = X_MARGIN;
+				element.y = Y_MARGIN + (i+2) * Y_LINE_OFFSET;
+				display_data.items.push_back(element);
 				std::string disp_string { };
 				if (item.is_value == true)
 				{
@@ -221,21 +244,21 @@ int main(int argc, char **argv)
 					disp_string = JSONParser::round_value(val, 1);
 				}
 				else
-				{
 					disp_string = JSONParser::get_string(weather_data, item.id);
-				}
 				disp_string += item.unit;
+				if (i == 0)
+					element.x = X_MARGIN;
+				else if (i == NUM_WEATHER_ITEMS - 1)
+					element.x = X_MARGIN_STATION;
+				else
+					element.x = X_MARGIN_SECOND_ROW;
 				element.text = disp_string;
-				element.y = element.y + Y_LINE_OFFSET;
-				weatherdata.items.push_back(element);
-				window1->update(&weatherdata);
-				weather_cycle++;
-				if (weather_cycle >= NUM_WEATHER_ITEMS)
-					weather_cycle = 0;
+				display_data.items.push_back(element);
 			}
-			else
-				continue; //If no weather data available, display date & time only
 		}
+			
+		//Update window data
+		window1->update(&display_data);
 		std::this_thread::sleep_for(std::chrono::seconds(SECONDS_WAIT));
 		main_cycle++;
 	}
